78c9fab8c96a41f5582ce949ba47f17171633ed3
tcodes9949 <tcodes9949@gmail.com>
2026-09-25 12:57:23 -0400

    Create functions.js
    Unit 4: add functions practice

---
+++ projects/js-practice/functions.js
@@ -0,0 +1,21 @@
+// Greeting Generator
+function greet(name) {
+  return "Hello " + name
+}
+ let message = greet("Tom")
+  console.log(message)
+
+
+// Returns the sum of numbers
+function add(a, b) {
+  return a + b
+}
+let total = add(10,90)
+console.log(total)
+
+// Checks if number is even 
+function isEven(num) {
+  return num % 2 === 0
+}
+console.log(isEven(5))
+console.log(isEven(8))
