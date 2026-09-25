b42c67415ea473f3d1ba81db4951ffc55fba6b7c
tcodes9949 <tcodes9949@gmail.com>
2026-09-25 02:50:59 -0400

    Create loops.js

---
+++ projects/js-practice/loops.js
@@ -0,0 +1,9 @@
+for ( let x= 1; x <= 10; x++) {      // Loop 1: count 1 to 10
+    console.log(x);
+}
+
+for ( let i = 1; i <= 10; i++ ) {    // Loop 2: even numbers 1 to 10
+  if (i % 2 === 0) {
+  console.log(i);
+  }
+}
