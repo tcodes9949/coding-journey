c08ac9310a2f744d8ae9484aa1c43596b3683f1e
tcodes9949 <tcodes9949@gmail.com>
2026-09-24 20:55:24 -0400

    Unit 2: add temperature checker

--- projects/js-practice/conditionals.js
+++ projects/js-practice/conditionals.js
@@ -1,11 +1,12 @@
-//let establishes the variable to test conditions against (temp=30)
+//temp to test our conditions against
 let temp = 30 
 //is temp greater than 25? 
 if (temp > 25) {
 //statement - prints "It's hot"
   console.log("It's hot") 
+} 
 //`else if` "if previous check was false, try this one instead"; (temp>= 15) "is temp 15 or more?"
-} else if (temp >= 15) {
+else if (temp >= 15) {
 //run this if thats `true`
   console.log("It's nice")
 //no condition. Run if everything above was false
