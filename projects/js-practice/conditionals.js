2dbcf853123ef32a11d6ed2d774a911c3272399b
tcodes9949 <tcodes9949@gmail.com>
2026-09-24 20:51:07 -0400

    Update conditionals.js

--- projects/js-practice/conditionals.js
+++ projects/js-practice/conditionals.js
@@ -1,9 +1,15 @@
-let temp = 30 // let establishes the the variable to test conditions against (temp=30)
-
-if (temp > 25){ // `if` starts a conditiona; (temp>25") - Condition/boolean `true`or `false` ; {} code block - the instructions to run if the condition is `true`
-  console.log("It's hot") // statement - prints "It's hot"
-} els if (temp >= 15){ //`else if` "if previous check was false, try this one instead"; (temp>= 15) "is temp 15 or more?"
-  console.log("It's nice") // run this if thats `true`
-} else { // no condition. Run if everything above was false
-  console.log("It's cold") // the fallback
+//let establishes the variable to test conditions against (temp=30)
+let temp = 30 
+//is temp greater than 25? 
+if (temp > 25) {
+//statement - prints "It's hot"
+  console.log("It's hot") 
+//`else if` "if previous check was false, try this one instead"; (temp>= 15) "is temp 15 or more?"
+} else if (temp >= 15) {
+//run this if thats `true`
+  console.log("It's nice")
+//no condition. Run if everything above was false
+} else { 
+//the fallback
+  console.log("It's cold") 
 }
