f21d4f9c904f599879a4268ab84c305a099d77db
tcodes9949 <tcodes9949@gmail.com>
2026-09-25 23:39:55 -0400

    Update objects.js

--- projects/js-practice/objects.js
+++ projects/js-practice/objects.js
@@ -1,1 +1,14 @@
 
+let contacts = [
+{name: "Jeff", age: 40, email: "gmail"},
+{name: "Tim", age: 49, email: "gmail"},
+{name: "Bret", age: 36, email: "yahoo"}
+]
+
+  console.log(contacts[1].name)
+  console.log(contacts[0].email)
+ 
+for ( let i = 0; i < contacts.length; i++) {
+  console.log(
+  contacts[i].name + " - "  + contacts[i].email)
+}
